f45e7a0ba3a742fb45cb7094951d1d5d67351012
olga210600 <89257451+olga210600@users.noreply.github.com>
2021-12-13 22:20:44 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import {Controller} from "./modules";
+// import {View} from "./modules";
+import './style.css';
+//
+// const input = document.getElementById('add-item');
+//
+// export const controller = new Controller();
+// export const view = new View(controller.model);
+//
+// input.onkeypress = (e) => view.addItem(e);
+//
+// controller.Init()
+
+alert('hellobbb')
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: './dist',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        esmodules: true,
+                                    },
+                                },
+                            ],
+                        ],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+    ],
+};
+
